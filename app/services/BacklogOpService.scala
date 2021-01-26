5d1155c26685c488fe3aa1b0e4ccab3677ba1628
*RaTnwT3lk0 <chcyang202@gmail.com>
2021-01-26 18:27:51 +0900

    [update]temporary commit for checkout a other branch

--- app/services/BacklogOpService.scala
+++ app/services/BacklogOpService.scala
@@ -2,9 +2,12 @@ package services
 
 import com.google.inject.{Inject, Singleton}
 import config.BacklogGateWayConfig
+import exception.AppException
 import gateways.{Params, WebService}
 import org.slf4j.{Logger, LoggerFactory}
 import play.api.http.Status
+import play.api.libs.ws.WSResponse
+import utils.{BaseClientError, BaseSystemFailure}
 
 import scala.concurrent.Future
 
@@ -13,7 +16,7 @@ class BacklogOpService @Inject()(gateWayConfig: BacklogGateWayConfig, webService
 
   final private val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
-  def getSpace() = {
+  def getSpace(): Future[String] = {
     val endpoint = gateWayConfig.baseUrl + gateWayConfig.spaceApiPath
     val params = Params("apiKey" -> gateWayConfig.backlogApiKey)
     webService.getResponseWithHeaders(endpoint, timeout = gateWayConfig.serviceTimeout, params = params) {
@@ -26,4 +29,31 @@ class BacklogOpService @Inject()(gateWayConfig: BacklogGateWayConfig, webService
     }
   }
 
+  def getSharedFiles():Future[String]  = {
+
+    val endpoint = gateWayConfig.baseUrl + gateWayConfig.spaceApiPath
+    val params = Params("apiKey" -> gateWayConfig.backlogApiKey,
+      "order"->"asc",
+      "offset"->"123",
+      "count"-> "1000"
+    )
+    webService.getResponseWithHeaders(endpoint, params = params) (esResponseTransform)
+  }
+
+  private def esResponseTransform(response: WSResponse) = {
+    response.status match {
+      case status if Status.isSuccessful(status) =>
+        logger.debug(response.body)
+        Future.successful(response.body)
+      case status if Status.isClientError(status) =>
+        logger.error(response.body)
+        throw new AppException(BaseClientError(response.body))
+      case status if Status.isServerError(status) =>
+        logger.error(response.body)
+        throw new AppException(BaseSystemFailure(response.body))
+      case _ =>
+        logger.error(response.body)
+        throw new AppException(BaseSystemFailure(s"Elasticsearch other status:${response.body}"))
+    }
+  }
 }
